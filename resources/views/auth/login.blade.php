42042b77344d5479b6700ca8782325d62a033773
Afy Wahyu <afyww18@gmail.com>
2024-01-12 22:41:56 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,8 +15,7 @@
             <div class="relative flex items-center p-0 overflow-hidden bg-center bg-cover min-h-75-screen">
                 <div class="container z-10">
                     <div class="flex flex-wrap mt-0 -mx-3">
-                        <div class="flex flex-col w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-6/12 lg:w-5/12 xl:w-4/12 2xl:w-full ">
-                            <!-- ... -->
+                        <div class="flex flex-col w-full px-3 mx-auto md:flex-0 shrink-0 md:w-6/12 lg:w-5/12 xl:w-4/12 2xl:w-5/12 ">
                             <div class="relative flex flex-col min-w-0 mt-32 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border">
                                 <div class="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
                                     <h3 class="relative z-10 text-3xl font-bold text-transparent bg-gradient-to-tl from-red-900 to-red-600 bg-clip-text">
@@ -48,11 +47,13 @@
                             </div>
                         </div>
                         <div class="w-full max-w-full px-3 lg:flex-0 shrink-0 md:w-6/12">
-                            <div class="absolute top-0 hidden w-3/5 h-full -mr-32 overflow-hidden -skew-x-10 -right-40 rounded-bl-xl md:block">
-                                <div class="relative inset-x-0 top-0 z-0 h-full -ml-15 bg-fixed skew-x-12"
-                                    style="background-image: url('{{ asset('assets/img/gedungg.jpg') }}');"></div>
+                            <div class="absolute top-0 hidden xl:w-4/5 2xl:w-3/5 h-full -mr-32 overflow-hidden -right-40 rounded-bl-xl md:block">
+                                <div class="inset-x-0 top-0 z-0 h-full -ml-15 bg-fixed skew-x-12">
+                                    <img src="{{ asset('assets/img/gedungg.jpg') }}" alt="Your Image Alt Text" style="object-fit: contain; width: 100%; height: 100%;">
+                                </div>
                             </div>
                         </div>
+                        
                     </div>
                 </div>
             </div>
