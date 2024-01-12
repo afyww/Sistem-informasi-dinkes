a87c19e8c01597c3e54e82da79256de854217121
Afy Wahyu <afyww18@gmail.com>
2024-01-12 10:48:48 +0700

    push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,9 +60,9 @@
     </main>
     @include('sweetalert::alert')
     <!-- plugin for scrollbar  -->
-    <script src="../assets/js/plugins/perfect-scrollbar.min.js" async></script>
+    <script src="{{ asset('../assets/js/plugins/perfect-scrollbar.min.js') }}" async></script>
     <!-- main script file  -->
-    <script src="../assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5" async></script>
+    <script src="{{ asset('../assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5') }}" async></script>
 </body>
 
 </html>
